dc8d54bd5bea6926e70f73e411d55db7d6d98643
Danielle Heberling <danielle.heberling@navabenefits.com>
2023-02-18 09:37:09 -0800

    Documentation

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # countdown
-A CLI util to determine how many days are left until a certain date
+
+A CLI util to determine how many <units of time> are left until a certain date from the current date.
+
+## Usage
+
+1. Make sure you have node 18 or higher installed (see `.nvmrc`)
+2. Clone this repo and run `npm install`
+3. Run `npm start` to start the CLI
+
+## Options
+
+By default the CLI will show the number of days until 08-15-2023. You can change this by passing in a date (`-d` or `--date`) as an argument. The date should be in the format `YYYY-MM-DD` or `MM-DD-YYYY`. Dates in the past will return a negative number.
+
+If you want to see the amount of time in a different unit than days, you can pass in a second argument (`-u` or `--unit`). The second argument should be one of the following:
+
+- milliseconds
+- seconds
+- minutes
+- hours
+- days
+- weeks
+- months
+- years
+
+## Examples
+
+1. Show the number of days until 02-23-2023
+
+```bash
+npx ts-node index.ts countdown -d 02-23-2023
+```
+
+2. Show the number of hours until 02-23-2023
+
+```bash
+npx ts-node index.ts countdown -d 02-23-2023 -u hours
+```
+
+## Contributing
+
+New features or bugfixes are welcome. Please open an issue or PR. See [CONTRIBUTING.md](CONTRIBUTING.md) for more details.
--- index.ts
+++ index.ts
@@ -1,11 +1,60 @@
 import { DateTime } from 'luxon'
+import { Command } from 'commander'
 
-const currentDate = new Date()
-const targetDate = new Date('2023-08-15')
+type Options = { date: string; unit: string }
+type TimeType =
+  | 'milliseconds'
+  | 'seconds'
+  | 'minutes'
+  | 'hours'
+  | 'days'
+  | 'weeks'
+  | 'months'
+  | 'years'
 
-const daysLeft = DateTime.fromJSDate(targetDate).diff(
-  DateTime.fromJSDate(currentDate),
-  ['days'],
-)
+const program = new Command()
 
-console.log(`${Math.round(daysLeft.days)} days left. 😎`)
+program
+  .command('countdown')
+  .description('Get time left until a date from today')
+  .option('-d, --date <date>')
+  .option('-u, --unit <unit>')
+  .action((options: Options) => {
+    const { date, unit } = options
+
+    const currentDate = new Date()
+    const targetDate = date ? new Date(options.date) : new Date('2023-08-15')
+    const outputUnit = unit ?? 'days'
+
+    const daysLeft = DateTime.fromJSDate(targetDate).diff(
+      DateTime.fromJSDate(currentDate),
+      [outputUnit as TimeType],
+    )
+
+    function outputHelper(): number {
+      switch (outputUnit) {
+        case 'milliseconds':
+          return daysLeft.milliseconds
+        case 'seconds':
+          return daysLeft.seconds
+        case 'minutes':
+          return daysLeft.minutes
+        case 'hours':
+          return daysLeft.hours
+        case 'days':
+          return daysLeft.days
+        case 'weeks':
+          return daysLeft.weeks
+        case 'months':
+          return daysLeft.months
+        case 'years':
+          return daysLeft.years
+        default:
+          throw new Error('Invalid duration type')
+      }
+    }
+
+    console.log(`${Math.round(outputHelper())} ${outputUnit} left. 😎`)
+  })
+
+program.parse()
